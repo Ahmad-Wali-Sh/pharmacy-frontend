af6f8fa44713238818bd85d214042a78ad0ba89f
Ahmad Wali <ahmadwalisharifybtc@gmail.com>
2023-05-04 16:56:12 +0430

    Sell Prescription Evolved

--- src/components/Sell/Sell.jsx
+++ src/components/Sell/Sell.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Modal from "react-modal";
+import Prescription from "./Prescription/Prescription";
 
 Modal.setAppElement("#root");
 
@@ -7,7 +8,7 @@ function Sell() {
   return (
     <div className="purchase">
       <div className="purchase-box">
-        
+        <Prescription title="ثبت نسخه" icon="fa-solid fa-cart-arrow-down"/>
       </div>
       <div className="purchase-form"></div>
     </div>
