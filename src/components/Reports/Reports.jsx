6c6c65bb71058f18efe1a15b2b57ed78eac6280f
Ahmad Wali <ahmadwalisharifybtc@gmail.com>
2023-05-14 16:29:33 +0430

    Medician Reporting Finished

--- src/components/Reports/Reports.jsx
+++ src/components/Reports/Reports.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Modal from "react-modal";
-import BaseLists from "./BaseLists";
+import MedicineBaseLists from "./MedicineLists/MedicineBaseLists";
 
 Modal.setAppElement("#root");
 
@@ -8,7 +8,7 @@ function Reports() {
   return (
     <div className="purchase">
       <div className="purchase-box">
-          <BaseLists title="موارد اولیه" icon="fa-solid fa-clipboard-list"/>
+          <MedicineBaseLists title="موارد اولیه" icon="fa-solid fa-clipboard-list"/>
       </div>
       <div className="purchase-form">
       </div>
