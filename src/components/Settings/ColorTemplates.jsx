8afaae2801d81597bad05e3f300a6aa7c89b8ab2
Ahmad Wali <ahmadwalisharifybtc@gmail.com>
2023-11-15 01:18:42 +0430

    template Colors assimble

---
+++ src/components/Settings/ColorTemplates.jsx
@@ -0,0 +1,122 @@
+import React, { useRef } from "react";
+import SmallModal from "../PageComponents/Modals/SmallModal";
+import { useTemplateLogo } from "../States/States";
+
+function ColorTemplates() {
+  const TemplateColorsRef = useRef(null);
+  const { templateLogo, setTemplateLogo } = useTemplateLogo();
+
+  let root = document.querySelector(":root");
+
+  const cyan = () => {
+    setTemplateLogo("./images/frontend/cyan-sharif.png");
+    root.style.setProperty("--color-one", "#009aa3");
+    root.style.setProperty("--color-two", "#006c72");
+    root.style.setProperty("--color-eight", "#006c72");
+    root.style.setProperty("--color-eleven", "#006c72");
+    root.style.setProperty("--color-twelve", "#006c72");
+    root.style.setProperty("--color-four", "rgb(170, 170, 170)");
+  };
+
+  const green = () => {
+    setTemplateLogo("./images/frontend/green-sharif.png");
+    root.style.setProperty("--color-one", "#1db853");
+    root.style.setProperty("--color-two", "#137b37");
+    root.style.setProperty("--color-eight", "#1db853");
+    root.style.setProperty("--color-eleven", "#1db853");
+    root.style.setProperty("--color-twelve", "#1db853");
+    root.style.setProperty("--color-four", "rgb(80, 80, 80)");
+  };
+
+  const red = () => {
+    setTemplateLogo("./images/frontend/red-sharif.png");
+    root.style.setProperty("--color-one", "#5f0609");
+    root.style.setProperty("--color-two", "#be0d12");
+    root.style.setProperty("--color-eight", "#5f0609");
+    root.style.setProperty("--color-eleven", "#5f0609");
+    root.style.setProperty("--color-twelve", "#5f0609");
+    root.style.setProperty("--color-four", "rgb(110, 110, 110)");
+  };
+
+  const yellow = () => {
+    setTemplateLogo("./images/frontend/yellow-sharif.png");
+    root.style.setProperty("--color-one", "#989802");
+    root.style.setProperty("--color-two", "#d1d102");
+    root.style.setProperty("--color-eight", "#989802");
+    root.style.setProperty("--color-eleven", "#989802");
+    root.style.setProperty("--color-twelve", "#989802");
+    root.style.setProperty("--color-four", "rgb(80, 80, 80)");
+  };
+
+  const purple = () => {
+    setTemplateLogo("./images/frontend/colorful.png");
+    root.style.setProperty("--color-one", "#533855");
+    root.style.setProperty("--color-two", "#6b4a99");
+    root.style.setProperty("--color-eight", "rgb(77, 37, 81)");
+    root.style.setProperty("--color-eleven", "rgb(74, 43, 80)");
+    root.style.setProperty("--color-twelve", "rgb(111, 63, 119)");
+    root.style.setProperty("--color-four", "rgb(110, 110, 110)");
+  };
+
+  const blue = () => {
+    setTemplateLogo("./images/frontend/blue-sharif.png");
+    root.style.setProperty("--color-one", "#063a92");
+    root.style.setProperty("--color-two", "#0754b6");
+    root.style.setProperty("--color-eight", "#063a88");
+    root.style.setProperty("--color-eleven", "#063a88");
+    root.style.setProperty("--color-twelve", "#063a88");
+    root.style.setProperty("--color-four", "rgb(110, 110, 110)");
+  };
+
+  const dark = () => {
+    root.style.setProperty("--color-thirth", "rgb(93, 93, 93)");
+    root.style.setProperty("--color-nine", "rgb(87, 84, 84)");
+    root.style.setProperty("--color-sex", "rgb(61, 61, 61)");
+    root.style.setProperty("--color-three", "rgb(34, 34, 34)");
+    root.style.setProperty("--color-ten", "white");
+  };
+
+  const light = () => {
+    root.style.setProperty("--color-thirth", "rgb(189, 189, 189)");
+    root.style.setProperty("--color-nine", "rgb(109, 109, 109)");
+    root.style.setProperty("--color-sex", "rgb(134, 130, 130)");
+    root.style.setProperty("--color-three", "rgb(187, 187, 187)");
+    root.style.setProperty("--color-ten", "rgb(0, 0, 0)");
+  };
+
+  return (
+    <>
+      <div className="icons" onClick={() => TemplateColorsRef.current.Opener()}>
+        <i class="fa-solid fa-droplet"></i>
+      </div>
+      <SmallModal ref={TemplateColorsRef} title="تغییر رنگ">
+        <div className="color-pallete-container">
+          <div className="color-pallete-item" onClick={() => purple()}>
+            <div className="color-pallete-purple"></div>
+          </div>
+          <div className="color-pallete-item" onClick={() => cyan()}>
+            <div className="color-pallete-cyan"></div>
+          </div>
+          <div className="color-pallete-item" onClick={() => green()}>
+            <div className="color-pallete-green"></div>
+          </div>
+          <div className="color-pallete-item" onClick={() => red()}>
+            <div className="color-pallete-red"></div>
+          </div>
+          <div className="color-pallete-item" onClick={() => blue()}>
+            <div className="color-pallete-blue"></div>
+          </div>
+          <div className="color-pallete-item" onClick={() => yellow()}>
+            <div className="color-pallete-yellow"></div>
+          </div>
+          <div className="dark-light-container">
+            <div className="color-dark" onClick={() => dark()}></div>
+            <div className="color-light" onClick={() => light()}></div>
+          </div>
+        </div>
+      </SmallModal>
+    </>
+  );
+}
+
+export default ColorTemplates;
--- src/styles.jsx
+++ src/styles.jsx
@@ -22,7 +22,7 @@ export const ModalBigStyles = {
     overflow: "hidden",
     padding: "0px",
     margin: "0px",
-    zIndex: 10
+    zIndex: 10,
   },
   overlay: {
     backgroundColor: "rgba(60,60,60,0.5)",
@@ -33,15 +33,15 @@ export const MedicineShowModalStyles = {
   content: {
     backgroundColor: "rgb(30,30,30)",
     border: "none",
-    width: '85%',
-    height: '30rem',
-    top:'14%',
-    left: '7%',
+    width: "85%",
+    height: "30rem",
+    top: "14%",
+    left: "7%",
     borderRadius: "1rem",
     overflow: "hidden",
     padding: "0px",
     margin: "0px",
-    zIndex: 10
+    zIndex: 10,
   },
   overlay: {
     backgroundColor: "rgba(60,60,60,0.5)",
@@ -82,102 +82,101 @@ export const AutoCompleteStyle = {
 export const MedicineSelectStyle = {
   control: (baseStyles, state) => ({
     ...baseStyles,
-    backgroundColor: 'rgb(30,30,30)',
-    color: "white",
-    border: 'none',
-    borderRadius: '1rem',
-    boxShadow: state.isFocused && '-4px -4px 4px var(--color-two);'
+    backgroundColor: "var(--color-three)",
+    color: "var(--color-ten)",
+    border: "none",
+    borderRadius: "1rem",
+    boxShadow: state.isFocused && "-4px -4px 4px var(--color-two);",
   }),
   option: (styles, state) => ({
     ...styles,
-    backgroundColor: state.isFocused ? 'rgb(80,80,80)': 'rgb(40,40,40)'
+    backgroundColor: state.isFocused ? "var(--color-nine)" : "var(--color-sex)",
   }),
   container: (styles) => ({
     ...styles,
-    backgroundColor: 'rgb(60,60,60)',
+    backgroundColor: "var(--color-three)",
   }),
   input: (styles) => ({
     ...styles,
-    color: 'white'
+    color: "var(--color-ten)",
   }),
   noOptionsMessage: (styles) => ({
     ...styles,
-    display: 'none'
+    display: "none",
   }),
   menuList: (styles) => ({
     ...styles,
-    backgroundColor: 'rgb(60,60,60)',
-    maxHeight: '70vh'
-  })
-}
+    backgroundColor: "var(--color-three)",
+    maxHeight: "70vh",
+  }),
+};
 
 export const SelectInputStyle = {
   control: (baseStyles, state) => ({
     ...baseStyles,
-    backgroundColor: 'rgb(30,30,30)',
+    backgroundColor: "var(--color-three)",
     color: "white",
-    minHeight: '1.3rem',
-    height: '1.5rem',
-    border: 'none',
-    borderRadius: '1rem',
-    boxShadow: state.isFocused && '-4px -4px 4px var(--color-two);',
-    width: '100%'
+    minHeight: "1.3rem",
+    height: "1.5rem",
+    border: "none",
+    borderRadius: "1rem",
+    boxShadow: state.isFocused && "-4px -4px 4px var(--color-two);",
+    width: "100%",
   }),
   indicatorsContainer: (base, state) => ({
     ...base,
-    minHeight: '1.3rem',
-    height: '1.8rem',
-    border: 'none',
-    width: '23%'
+    minHeight: "1.3rem",
+    height: "1.8rem",
+    border: "none",
+    width: "23%",
   }),
   container: (base, state) => ({
     ...base,
-    width: '100%',
+    width: "100%",
   }),
   indicatorsDropdown: (base) => ({
     ...base,
-    border: 'none',
+    border: "none",
   }),
   menu: (base, state) => ({
     ...base,
-    backgroundColor: 'rgb(30,30,30)',
+    backgroundColor: "var(--color-sex)",
   }),
   menuList: (base, state) => ({
     ...base,
-    backgroundColor: state.isFocused ? 'rgb(60,60,60)' : '',
-    textAlign: 'right',
+    backgroundColor: state.isFocused ? "var(--color-nine)" : "",
+    textAlign: "right",
   }),
   menuPortal: (base) => ({
     ...base,
-    zIndex: 999
+    zIndex: 999,
   }),
   option: (base, state) => ({
     ...base,
-    backgroundColor: state.isFocused ? 'rgb(60,60,60)' : 'rgb(30,30,30)',
-    color: 'var(--color-ten)',
+    backgroundColor: state.isFocused ? "var(--color-nine)" : "var(--color-sex)",
+    color: "var(--color-ten)",
   }),
   input: (base, state) => ({
     ...base,
-    height: '1rem',
-    direction: 'rtl',
-    color: 'var(--color-ten)',
-    fontSize: '0.9rem',
-    marginTop: '-0.3rem',
+    height: "1rem",
+    direction: "rtl",
+    color: "var(--color-ten)",
+    fontSize: "0.9rem",
+    marginTop: "-0.3rem",
   }),
   singleValue: (base, state) => ({
     ...base,
-    direction: 'rtl',
-    textAlign: 'right',
-    color: 'var(--color-ten)',
+    direction: "rtl",
+    textAlign: "right",
+    color: "var(--color-ten)",
   }),
   multiValue: (base, state) => ({
     ...base,
-    backgroundColor: 'rgb(100,100,100)',
-    color: 'var(--color-ten)',
-    minHeight: '0.6rem',
-    height: '1.2rem',
-    position: 'relative',
-    top: '-0.2rem'
-  })
-}
-
+    backgroundColor: "var(--color-nine)",
+    color: "var(--color-ten)",
+    minHeight: "0.6rem",
+    height: "1.2rem",
+    position: "relative",
+    top: "-0.2rem",
+  }),
+};
