3b23621dc184bc812ff1657fada73d8056e39a13
Ahmad Wali Sharify <ahmadwalisharifybtc@gmail.com>
2024-03-01 22:09:55 +0430

    save able themes for further use.

--- src/components/Settings/ColorTemplates.jsx
+++ src/components/Settings/ColorTemplates.jsx
@@ -1,120 +1,149 @@
-import React, { useRef } from "react";
+import React, { useEffect, useRef } from "react";
 import SmallModal from "../PageComponents/Modals/SmallModal";
 import { useTemplateLogo } from "../States/States";
-import './color-template.css'
+import "./color-template.css";
 
 function ColorTemplates() {
   const TemplateColorsRef = useRef(null);
   const { templateLogo, setTemplateLogo } = useTemplateLogo();
 
+    // Function to get theme from localStorage and apply it
+    const applySavedTheme = () => {
+      const savedTheme = localStorage.getItem('theme');
+      if (savedTheme) {
+        const theme = JSON.parse(savedTheme);
+        applyTheme(theme);
+      }
+    };
+
+    // Function to apply theme
+    const applyTheme = (theme) => {
+      const root = document.documentElement;
+      Object.keys(theme).forEach((key) => {
+        root.style.setProperty(key, theme[key]);
+      });
+    };
+
+    // Function to save theme in localStorage
+    const saveTheme = (theme) => {
+      localStorage.setItem('theme', JSON.stringify(theme));
+    };
+
+  useEffect(() => {
+
+    // Apply saved theme when component mounts
+    applySavedTheme();
+
+    // Cleanup function
+    return () => {
+      // Any cleanup code if needed
+    };
+  }, []);
+
   let root = document.querySelector(":root");
 
   const cyan = () => {
-    setTemplateLogo("./images/frontend/cyan-sharif.png");
-    root.style.setProperty("--primary-100", "#0085ff");
-    root.style.setProperty("--primary-200", "#69b4ff");
-    root.style.setProperty("--primary-300", "#e0ffff");
-
-    root.style.setProperty("--bg-100", "#1E1E1E");
-    root.style.setProperty("--bg-200", "#2d2d2d");
-
-    root.style.setProperty("--bg-300", "#454545");
-    root.style.setProperty("--text-100", "#FFFFFF");
-    root.style.setProperty("--text-200", "#9e9e9e");
-    root.style.setProperty("--accent-100", "#006fff");
-    root.style.setProperty("--accent-200", "#e1ffff");
-
+    const theme = {
+      "--primary-100": "#0085ff",
+      "--primary-200": "#69b4ff",
+      "--primary-300": "#e0ffff",
+      "--bg-100": "#1E1E1E",
+      "--bg-200": "#2d2d2d",
+      "--bg-300": "#454545",
+      "--text-100": "#FFFFFF",
+      "--text-200": "#9e9e9e",
+      "--accent-100": "#006fff",
+      "--accent-200": "#e1ffff"
+    };
+    applyTheme(theme);
+    saveTheme(theme);
   };
 
   const green = () => {
-    setTemplateLogo("./images/frontend/green-sharif.png");
-    root.style.setProperty("--primary-100", "#7FB3D5");
-    root.style.setProperty("--primary-200", "#6296b7");
-    root.style.setProperty("--primary-300", "#195775");
-    root.style.setProperty("--bg-100", "#F5F5DC");
-    root.style.setProperty("--bg-200", "#ebebd2");
-    root.style.setProperty("--bg-300", "#c2c2aa");
-    root.style.setProperty("--text-100", "#333333");
-    root.style.setProperty("--text-200", "#5c5c5c");
-    root.style.setProperty("--accent-100", "#F7CAC9");
-    root.style.setProperty("--accent-200", "#926b6a");
-
+    const theme = {
+      "--primary-100": "#7FB3D5",
+      "--primary-200": "#6296b7",
+      "--primary-300": "#195775",
+      "--bg-100": "#F5F5DC",
+      "--bg-200": "#ebebd2",
+      "--bg-300": "#c2c2aa",
+      "--text-100": "#333333",
+      "--text-200": "#5c5c5c",
+      "--accent-100": "#F7CAC9",
+      "--accent-200": "#926b6a"
+    };
+    applyTheme(theme);
+    saveTheme(theme);
   };
 
   const red = () => {
-    setTemplateLogo("./images/frontend/red-sharif.png");
-    root.style.setProperty("--primary-100", "#596E79");
-    root.style.setProperty("--primary-200", "#788e98");
-    root.style.setProperty("--primary-300", "#F0F5F9");
-    root.style.setProperty("--bg-100", "#F0ECE3");
-    root.style.setProperty("--bg-200", "#DFD3C3");
-    root.style.setProperty("--bg-300", "#C7B198");
-    root.style.setProperty("--text-100", "#1E2022");
-    root.style.setProperty("--text-200", "#52616B");
-    root.style.setProperty("--accent-100", "#788189");
-    root.style.setProperty("--accent-200", "#e1e4e6");
+    const theme = {
+      "--primary-100": "#596E79",
+      "--primary-200": "#788e98",
+      "--primary-300": "#F0F5F9",
+      "--bg-100": "#F0ECE3",
+      "--bg-200": "#DFD3C3",
+      "--bg-300": "#C7B198",
+      "--text-100": "#1E2022",
+      "--text-200": "#52616B",
+      "--accent-100": "#788189",
+      "--accent-200": "#e1e4e6"
+    };
+    applyTheme(theme);
+    saveTheme(theme);
   };
 
   const yellow = () => {
-    setTemplateLogo("./images/frontend/yellow-sharif.png");
-    root.style.setProperty("--primary-100", "#00A896");
-    root.style.setProperty("--primary-200", "#008b7a");
-    root.style.setProperty("--primary-300", "#09554c");
-    root.style.setProperty("--bg-100", "#F2EFE9");
-    root.style.setProperty("--bg-200", "#e8e5df");
-    root.style.setProperty("--bg-300", "#bfbdb7");
-    root.style.setProperty("--text-100", "#333333");
-    root.style.setProperty("--text-200", "#5c5c5c");
-    root.style.setProperty("--accent-100", "#FF6B6B");
-    root.style.setProperty("--accent-200", "#8f001a");
-
+    const theme = {
+      "--primary-100": "#00A896",
+      "--primary-200": "#008b7a",
+      "--primary-300": "#09554c",
+      "--bg-100": "#F2EFE9",
+      "--bg-200": "#e8e5df",
+      "--bg-300": "#bfbdb7",
+      "--text-100": "#333333",
+      "--text-200": "#5c5c5c",
+      "--accent-100": "#FF6B6B",
+      "--accent-200": "#8f001a"
+    };
+    applyTheme(theme);
+    saveTheme(theme);
   };
 
   const purple = () => {
-    setTemplateLogo("./images/frontend/colorful.png");
-    root.style.setProperty("--primary-100", "#6c35de");
-    root.style.setProperty("--primary-200", "#a364ff");
-    root.style.setProperty("--primary-300", "#ffc7ff");
-    root.style.setProperty("--bg-100", "#241b35");
-    root.style.setProperty("--bg-200", "#342a45");
-    root.style.setProperty("--bg-300", "#4d425f");
-    root.style.setProperty("--text-100", "#ffffff");
-    root.style.setProperty("--text-200", "#e0e0e0");
-    root.style.setProperty("--accent-100", "#6e28a0");
-    root.style.setProperty("--accent-200", "#373737");
-
+    const theme = {
+      "--primary-100": "#6c35de",
+      "--primary-200": "#a364ff",
+      "--primary-300": "#ffc7ff",
+      "--bg-100": "#241b35",
+      "--bg-200": "#342a45",
+      "--bg-300": "#4d425f",
+      "--text-100": "#ffffff",
+      "--text-200": "#e0e0e0",
+      "--accent-100": "#6e28a0",
+      "--accent-200": "#373737"
+    };
+    applyTheme(theme);
+    saveTheme(theme);
   };
-
+  
   const blue = () => {
-    setTemplateLogo("./images/frontend/blue-sharif.png");
-    root.style.setProperty("--primary-100", "#B0C5D1");
-    root.style.setProperty("--primary-200", "#93a8b3");
-    root.style.setProperty("--primary-300", "#536771");
-    root.style.setProperty("--bg-100", "#E6F2EA");
-    root.style.setProperty("--bg-200", "#dce8e0");
-    root.style.setProperty("--bg-300", "#b4bfb8");
-    root.style.setProperty("--text-100", "#4D4D4D");
-    root.style.setProperty("--text-200", "#797979");
-    root.style.setProperty("--accent-100", "#D1B0C5");
-    root.style.setProperty("--accent-200", "#715467");
-
+    const theme = {
+      "--primary-100": "#B0C5D1",
+      "--primary-200": "#93a8b3",
+      "--primary-300": "#536771",
+      "--bg-100": "#E6F2EA",
+      "--bg-200": "#dce8e0",
+      "--bg-300": "#b4bfb8",
+      "--text-100": "#4D4D4D",
+      "--text-200": "#797979",
+      "--accent-100": "#D1B0C5",
+      "--accent-200": "#715467"
+    };
+    applyTheme(theme);
+    saveTheme(theme);
   };
 
-  const dark = () => {
-    root.style.setProperty("--color-thirth", "rgb(93, 93, 93)");
-    root.style.setProperty("--color-nine", "rgb(87, 84, 84)");
-    root.style.setProperty("--color-sex", "rgb(61, 61, 61)");
-    root.style.setProperty("--color-three", "rgb(34, 34, 34)");
-    root.style.setProperty("--color-ten", "white");
-  };
-
-  const light = () => {
-    root.style.setProperty("--color-thirth", "rgb(189, 189, 189)");
-    root.style.setProperty("--color-nine", "rgb(109, 109, 109)");
-    root.style.setProperty("--color-sex", "rgb(134, 130, 130)");
-    root.style.setProperty("--color-three", "rgb(187, 187, 187)");
-    root.style.setProperty("--color-ten", "rgb(0, 0, 0)");
-  };
 
   return (
     <>
@@ -141,7 +170,6 @@ function ColorTemplates() {
           <div className="color-pallete-item" onClick={() => yellow()}>
             <div className="color-pallete-yellow"></div>
           </div>
-
         </div>
       </SmallModal>
     </>
