e91fd1e0554ed2d2897c1c86c27195c3870700bc
Ahmad Wali Sharify <ahmadwalisharifybtc@gmail.com>
2024-03-18 19:09:07 +0430

    today date showing on top

--- src/components/PageComponents/Header.jsx
+++ src/components/PageComponents/Header.jsx
@@ -6,9 +6,20 @@ import ColorTemplates from "../Settings/ColorTemplates";
 import Settings from "../Settings/Settings";
 import ShortcutListener from "../Settings/ShortcutListener";
 import useServerIP from "../services/ServerIP";
+import moment from 'jalali-moment';
 
 
 function Header() {
+
+  const [time, setTime] = useState(moment().format('jYYYY-jMM-jDD | hh:mm A'));
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setTime(moment().format('jYYYY-jMM-jDD | hh:mm A'));
+    }, 60000); // update every minute
+
+    return () => clearInterval(timer); // clear interval on unmount
+  }, []); // empty dependency array means this effect runs once on mount and clean up on unmount
 
   const isAuthenticated = useIsAuthenticated();
   const signOut = useSignOut();
@@ -57,13 +68,21 @@ function Header() {
         </div>
         <div className="flex-item"></div>
         <div>
-          <div className="search-input-box">
+          {/* <div className="search-input-box">
             <input
               type="search"
               placeholder="Search"
               className="search-input"
             />
             <i className="fa-solid fa-magnifying-glass"></i>
+          </div> */}
+          <div className='today-date' style={{display:'flex',gap:'1rem'}}>
+            <div>
+                {time}            
+            </div>
+            <div style={{direction:'rtl'}}>
+                امروز:              
+            </div>
           </div>
         </div>
       </div>
