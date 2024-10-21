891fdab486b6e3d3f0e8ded0155ccbafff4eda49
Ahmad Wali Sharify <ahmadwalisharifybtc@gmail.com>
2024-10-21 18:18:18 +0430

    printing Settings

--- src/components/Sell/Prescription/Prescription/PrescriptionDetailedPrint.jsx
+++ src/components/Sell/Prescription/Prescription/PrescriptionDetailedPrint.jsx
@@ -1,65 +1,121 @@
-import React from "react";
-import moment from 'jalali-moment';
+import React, { useEffect, useState } from "react";
+import moment from "jalali-moment";
 import { useAuthUser } from "react-auth-kit";
+import useServerIP from "../../../services/ServerIP";
+import axios from "axios";
 
-const PrescriptionDetailedPrint = React.forwardRef(({prescriptionThrough, prescription, barcode}, ref) => {
-
+const PrescriptionDetailedPrint = React.forwardRef(
+  ({ prescriptionThrough, prescription, barcode }, ref) => {
+    const [globalSettings, setGlobalSettings] = useState({});
+    const { serverIP } = useServerIP();
+    const getGlobalSettings = () => {
+      axios.get(`${serverIP}api/global-settings/`).then((res) => {
+        setGlobalSettings(res.data);
+      });
+    };
+    useEffect(() => {
+      serverIP && getGlobalSettings();
+    }, [serverIP]);
     const user = useAuthUser();
-  return (
-    <div className='detailed-print-container' ref={ref}>
-        <div className="title">دواخانه شریف</div>
+    return (
+      <div className="detailed-print-container" ref={ref}>
+        <div className="title">{globalSettings?.detailed_fields?.title}</div>
         <div className="details">
-            <h3>زمان</h3>
-            <h3 >{moment().format('hh:mm A')}</h3>
-            <h3>تاریخ</h3>
-            <h3>{moment().format('jYYYY-jMM-jDD')}</h3>
-            <h3>مریض</h3>
+          {globalSettings?.detailed_fields?.time && <h3>زمان</h3>}
+          {globalSettings?.detailed_fields?.time && (
+            <h3>{moment().format("hh:mm A")}</h3>
+          )}
+          {globalSettings?.detailed_fields?.date && <h3>تاریخ</h3>}
+          {globalSettings?.detailed_fields?.date && (
+            <h3>{moment().format("jYYYY-jMM-jDD")}</h3>
+          )}
+          {globalSettings?.detailed_fields?.patient_name && <h3>مریض</h3>}
+          {globalSettings?.detailed_fields?.patient_name && (
             <h3>{prescription?.patient_name}</h3>
+          )}
+          {globalSettings?.detailed_fields?.prescription_number && (
             <h3>شماره نسخه</h3>
+          )}
+          {globalSettings?.detailed_fields?.prescription_number && (
             <h3>{prescription?.prescription_number}</h3>
-            <h3>تخفیف</h3>
+          )}
+          {globalSettings?.detailed_fields?.discount_value && <h3>تخفیف</h3>}
+          {globalSettings?.detailed_fields?.discount_value && (
             <h3>{prescription?.discount_value}</h3>
-            <h3>اضافه قیمت</h3>
+          )}
+          {globalSettings?.detailed_fields?.over_value && <h3>اضافه قیمت</h3>}
+          {globalSettings?.detailed_fields?.over_value && (
             <h3>{prescription?.over_value}</h3>
+          )}
         </div>
-        <div className="title">اقلام</div>
+        <div className="title">{globalSettings?.detailed_fields?.medicine_title}</div>
         <div className="medicine-details">
+          <div className="medicine-details-item">
+            {globalSettings?.detailed_fields?.index && <h3>ردیف</h3>}
+            {globalSettings?.detailed_fields?.medicine_full && <h3>دارو</h3>}
+            {globalSettings?.detailed_fields?.quantity && <h3>تعداد</h3>}
+            {globalSettings?.detailed_fields?.each_price && <h3>قیمت فی</h3>}
+            {globalSettings?.detailed_fields?.total_price && <h3>قیمت کل</h3>}
+          </div>
+          {prescriptionThrough?.map((item, num) => (
             <div className="medicine-details-item">
-                <h3>ردیف</h3>
-                <h3>دارو</h3>
-                <h3>تعداد</h3>
-                <h3>قیمت فی</h3>
-                <h3>قیمت کل</h3>
-            </div>
-            {prescriptionThrough?.map((item, num) => (
-            <div className="medicine-details-item">
-                <h3>{num + 1}</h3>
+              {globalSettings?.detailed_fields?.index && <h3>{num + 1}</h3>}
+              {globalSettings?.detailed_fields?.medicine_full && (
                 <h3>{item.medicine_full}</h3>
+              )}
+              {globalSettings?.detailed_fields?.quantity && (
                 <h3>{item.quantity}</h3>
+              )}
+              {globalSettings?.detailed_fields?.each_price && (
                 <h3>{item.each_price}</h3>
+              )}
+              {globalSettings?.detailed_fields?.total_price && (
                 <h3>{item.total_price}</h3>
+              )}
             </div>
-            ))}
+          ))}
         </div>
         <div className="footer">
+          {globalSettings?.detailed_fields?.medicine_length && (
             <h3>تعداد اقلام</h3>
+          )}
+          {globalSettings?.detailed_fields?.medicine_length && (
             <h3>{prescriptionThrough?.length}</h3>
-            <h3>مجموع قیمت</h3>
+          )}
+          {globalSettings?.detailed_fields?.grand_total && <h3>مجموع قیمت</h3>}
+          {globalSettings?.detailed_fields?.grand_total && (
             <h3>{prescription?.grand_total}</h3>
+          )}
+          {globalSettings?.detailed_fields?.payment_value && (
             <h3>قابل پرداخت</h3>
-            <h3>{parseFloat(prescription?.grand_total) - parseFloat(prescription?.discount_value) + parseFloat(prescription?.over_value)}</h3>
- 
+          )}
+          {globalSettings?.detailed_fields?.payment_value && (
+            <h3>
+              {parseFloat(prescription?.grand_total) -
+                parseFloat(prescription?.discount_value) +
+                parseFloat(prescription?.over_value)}
+            </h3>
+          )}
         </div>
         <div className="flex">
-            <h3 style={{border:'none'}}>هدایت</h3>
-            <h3 style={{border:'none'}}>{user().first_name + ' ' + user()?.last_name}</h3>
+          {globalSettings?.detailed_fields?.over_by && (
+            <h3 style={{ border: "none" }}>هدایت</h3>
+          )}
+          {globalSettings?.detailed_fields?.over_by && (
+            <h3 style={{ border: "none" }}>
+              {user().first_name + " " + user()?.last_name}
+            </h3>
+          )}
         </div>
-        <div>
-            <h3 style={{border:'none'}}>بارکد</h3>
+        {globalSettings?.detailed_fields?.barcode && (
+          <div>
+            <h3 style={{ border: "none" }}>بارکد</h3>
             <div>{barcode}</div>
-            </div>
-    </div>
-  );
-});
+          </div>
+        )}
+      </div>
+    );
+  }
+);
 
 export default PrescriptionDetailedPrint;
---
+++ src/components/Settings/PrintingSettings.jsx
@@ -0,0 +1,445 @@
+import React, { useEffect, useState } from "react";
+import { DepartButton } from "../PageComponents/Buttons/Buttons";
+import { toast } from "react-toastify";
+import axios from "axios";
+import useServerIP from "../services/ServerIP";
+
+function PrintingSettings() {
+  const [globalSettings, setGlobalSettings] = useState({});
+  const { serverIP } = useServerIP();
+  const getGlobalSettings = () => {
+    axios.get(`${serverIP}api/global-settings/`).then((res) => {
+      setGlobalSettings(res.data);
+    });
+  };
+
+  useEffect(() => {
+    serverIP && getGlobalSettings();
+  }, [serverIP]);
+  return (
+    <>
+      <div className="listing-hight-print">
+        <h4 style={{ padding: "1rem" }}>برگه مربوطه صندوق:</h4>
+        <div className="ent-settings-container">
+          <label>عرض_کاغذ:</label>
+          <input
+            type="text"
+            className="text-input-standard"
+            value={globalSettings?.ticket_paper_width}
+            onChange={(e) => {
+              setGlobalSettings({
+                ...globalSettings,
+                ticket_paper_width: e.target.value,
+              });
+            }}
+          />
+          <label>پرنتر:</label>
+          <input
+            type="text"
+            className="text-input-standard"
+            value={globalSettings?.ticket_printer}
+            onChange={(e) => {
+              setGlobalSettings({
+                ...globalSettings,
+                ticket_printer: e.target.value,
+              });
+            }}
+          />
+          <lable></lable>
+          <lable></lable>
+          <div className="printer-settings-fields-container">
+            <label>عنوان: </label>
+            <input
+              type="text"
+              value={globalSettings?.ticket_fields?.title}
+              onChange={(e) => {
+                setGlobalSettings({
+                  ...globalSettings,
+                  ticket_fields: {
+                    ...globalSettings?.ticket_fields,
+                    title: e.target.value,
+                  },
+                });
+              }}
+            />
+            <label>زمان: </label>
+            <input
+              type="checkbox"
+              checked={globalSettings?.ticket_fields?.time}
+              onChange={(e) => {
+                setGlobalSettings({
+                  ...globalSettings,
+                  ticket_fields: {
+                    ...globalSettings?.ticket_fields,
+                    time: e.target.checked,
+                  },
+                });
+              }}
+            />
+            <label>شماره_نسخه: </label>
+            <input
+              type="checkbox"
+              checked={globalSettings?.ticket_fields?.prescription_number}
+              onChange={(e) => {
+                setGlobalSettings({
+                  ...globalSettings,
+                  ticket_fields: {
+                    ...globalSettings?.ticket_fields,
+                    prescription_number: e.target.checked,
+                  },
+                });
+              }}
+            />
+            <label>نوع: </label>
+            <input
+              type="checkbox"
+              checked={globalSettings?.ticket_fields?.department}
+              onChange={(e) => {
+                setGlobalSettings({
+                  ...globalSettings,
+                  ticket_fields: {
+                    ...globalSettings?.ticket_fields,
+                    department: e.target.checked,
+                  },
+                });
+              }}
+            />
+            <label>بارکد: </label>
+            <input
+              type="checkbox"
+              checked={globalSettings?.ticket_fields?.barcode}
+              onChange={(e) => {
+                setGlobalSettings({
+                  ...globalSettings,
+                  ticket_fields: {
+                    ...globalSettings?.ticket_fields,
+                    barcode: e.target.checked,
+                  },
+                });
+              }}
+            />
+            <label>قیمت: </label>
+            <input
+              type="checkbox"
+              checked={globalSettings?.ticket_fields?.payment}
+              onChange={(e) => {
+                setGlobalSettings({
+                  ...globalSettings,
+                  ticket_fields: {
+                    ...globalSettings?.ticket_fields,
+                    payment: e.target.checked,
+                  },
+                });
+              }}
+            />
+          </div>
+        </div>
+        <h4 style={{ padding: "1rem" }}>برگه اطلاعات اقلام :</h4>
+        <div className="printer-settings-fields-container-display">
+          <label>عرض_کاغذ:</label>
+          <input
+            type="text"
+            className="text-input-standard"
+            value={globalSettings?.detailed_paper_width}
+            onChange={(e) => {
+              setGlobalSettings({
+                ...globalSettings,
+                detailed_paper_width: e.target.value,
+              });
+            }}
+          />
+          <label>پرنتر:</label>
+          <input
+            type="text"
+            className="text-input-standard"
+            value={globalSettings?.detailed_printer}
+            onChange={(e) => {
+              setGlobalSettings({
+                ...globalSettings,
+                detailed_printer: e.target.value,
+              });
+            }}
+          />
+          <lable></lable>
+          <lable></lable>
+          <div className="printer-settings-fields-container">
+            <label>عنوان: </label>
+            <input
+              type="text"
+              value={globalSettings?.detailed_fields?.title}
+              onChange={(e) => {
+                setGlobalSettings({
+                  ...globalSettings,
+                  detailed_fields: {
+                    ...globalSettings?.detailed_fields,
+                    title: e.target.value,
+                  },
+                });
+              }}
+            />
+            <label>عنوان_اقلام: </label>
+            <input
+              type="text"
+              value={globalSettings?.detailed_fields?.medicine_title}
+              onChange={(e) => {
+                setGlobalSettings({
+                  ...globalSettings,
+                  detailed_fields: {
+                    ...globalSettings?.detailed_fields,
+                    medicine_title: e.target.value,
+                  },
+                });
+              }}
+            />
+          </div>
+          <div className="printer-settings-fields-container">
+            <label>زمان: </label>
+            <input
+              type="checkbox"
+              checked={globalSettings?.detailed_fields?.time}
+              onChange={(e) => {
+                setGlobalSettings({
+                  ...globalSettings,
+                  detailed_fields: {
+                    ...globalSettings?.detailed_fields,
+                    time: e.target.checked,
+                  },
+                });
+              }}
+            />
+            <label>تاریخ: </label>
+            <input
+              type="checkbox"
+              checked={globalSettings?.detailed_fields?.date}
+              onChange={(e) => {
+                setGlobalSettings({
+                  ...globalSettings,
+                  detailed_fields: {
+                    ...globalSettings?.detailed_fields,
+                    date: e.target.checked,
+                  },
+                });
+              }}
+            />
+            <label>مریض: </label>
+            <input
+              type="checkbox"
+              checked={globalSettings?.detailed_fields?.patient_name}
+              onChange={(e) => {
+                setGlobalSettings({
+                  ...globalSettings,
+                  detailed_fields: {
+                    ...globalSettings?.detailed_fields,
+                    patient_name: e.target.checked,
+                  },
+                });
+              }}
+            />
+            <label>تخفیف: </label>
+            <input
+              type="checkbox"
+              checked={globalSettings?.detailed_fields?.discount_value}
+              onChange={(e) => {
+                setGlobalSettings({
+                  ...globalSettings,
+                  detailed_fields: {
+                    ...globalSettings?.detailed_fields,
+                    discount_value: e.target.checked,
+                  },
+                });
+              }}
+            />
+            <label>اضافه_قیمت: </label>
+            <input
+              type="checkbox"
+              checked={globalSettings?.detailed_fields?.over_value}
+              onChange={(e) => {
+                setGlobalSettings({
+                  ...globalSettings,
+                  detailed_fields: {
+                    ...globalSettings?.detailed_fields,
+                    over_value: e.target.checked,
+                  },
+                });
+              }}
+            />
+            <label>ردیف: </label>
+            <input
+              type="checkbox"
+              disabled
+              checked={globalSettings?.detailed_fields?.index}
+              onChange={(e) => {
+                setGlobalSettings({
+                  ...globalSettings,
+                  detailed_fields: {
+                    ...globalSettings?.detailed_fields,
+                    index: e.target.checked,
+                  },
+                });
+              }}
+            />
+            <label>نام_دارو: </label>
+            <input
+              type="checkbox"
+              disabled
+              checked={globalSettings?.detailed_fields?.medicine_full}
+              onChange={(e) => {
+                setGlobalSettings({
+                  ...globalSettings,
+                  detailed_fields: {
+                    ...globalSettings?.detailed_fields,
+                    medicine_full: e.target.checked,
+                  },
+                });
+              }}
+            />
+            <label>تعداد: </label>
+            <input
+            disabled
+              type="checkbox"
+              checked={globalSettings?.detailed_fields?.quantity}
+              onChange={(e) => {
+                setGlobalSettings({
+                  ...globalSettings,
+                  detailed_fields: {
+                    ...globalSettings?.detailed_fields,
+                    quantity: e.target.checked,
+                  },
+                });
+              }}
+            />
+            <label>قیمت فی: </label>
+            <input
+              type="checkbox"
+              checked={globalSettings?.detailed_fields?.each_price}
+              onChange={(e) => {
+                setGlobalSettings({
+                  ...globalSettings,
+                  detailed_fields: {
+                    ...globalSettings?.detailed_fields,
+                    each_price: e.target.checked,
+                  },
+                });
+              }}
+            />
+            <label>قیمت کل: </label>
+            <input
+              type="checkbox"
+              checked={globalSettings?.detailed_fields?.total_price}
+              onChange={(e) => {
+                setGlobalSettings({
+                  ...globalSettings,
+                  detailed_fields: {
+                    ...globalSettings?.detailed_fields,
+                    total_price: e.target.checked,
+                  },
+                });
+              }}
+            />
+            <label>تعداد_اقلام: </label>
+            <input
+              type="checkbox"
+              checked={globalSettings?.detailed_fields?.medicine_length}
+              onChange={(e) => {
+                setGlobalSettings({
+                  ...globalSettings,
+                  detailed_fields: {
+                    ...globalSettings?.detailed_fields,
+                    medicine_length: e.target.checked,
+                  },
+                });
+              }}
+            />
+            <label>مجموع_قیمت: </label>
+            <input
+              type="checkbox"
+              checked={globalSettings?.detailed_fields?.grand_total}
+              onChange={(e) => {
+                setGlobalSettings({
+                  ...globalSettings,
+                  detailed_fields: {
+                    ...globalSettings?.detailed_fields,
+                    grand_total: e.target.checked,
+                  },
+                });
+              }}
+            />
+            <label>هدایت: </label>
+            <input
+              type="checkbox"
+              checked={globalSettings?.detailed_fields?.order_by}
+              onChange={(e) => {
+                setGlobalSettings({
+                  ...globalSettings,
+                  detailed_fields: {
+                    ...globalSettings?.detailed_fields,
+                    order_by: e.target.checked,
+                  },
+                });
+              }}
+            />
+            <label>شماره_نسخه: </label>
+            <input
+              type="checkbox"
+              checked={globalSettings?.detailed_fields?.prescription_number}
+              onChange={(e) => {
+                setGlobalSettings({
+                  ...globalSettings,
+                  detailed_fields: {
+                    ...globalSettings?.detailed_fields,
+                    prescription_number: e.target.checked,
+                  },
+                });
+              }}
+            />
+            <label>بارکد: </label>
+            <input
+              type="checkbox"
+              checked={globalSettings?.detailed_fields?.barcode}
+              onChange={(e) => {
+                setGlobalSettings({
+                  ...globalSettings,
+                  detailed_fields: {
+                    ...globalSettings?.detailed_fields,
+                    barcode: e.target.checked,
+                  },
+                });
+              }}
+            />
+            <label>قیمت: </label>
+            <input
+              type="checkbox"
+              checked={globalSettings?.detailed_fields?.payment_value}
+              onChange={(e) => {
+                setGlobalSettings({
+                  ...globalSettings,
+                  detailed_fields: {
+                    ...globalSettings?.detailed_fields,
+                    payment_value: e.target.checked,
+                  },
+                });
+              }}
+            />
+          </div>
+        </div>
+      </div>
+      <div className="list-footer">
+        <div>
+          <DepartButton
+            name="تایید"
+            Func={() => {
+              axios
+                .patch(`${serverIP}api/global-settings/`, globalSettings)
+                .then((res) => {
+                  setGlobalSettings(res.data);
+                  toast.success("تنظیمات موفقانه ذخیره شد");
+                });
+            }}
+          />
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default PrintingSettings;
